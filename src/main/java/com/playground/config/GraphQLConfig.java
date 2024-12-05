9d4a1d0806bd5573b2f42cdac6e8ac66f33ef588
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-05 15:56:19 -0500

    Add config for extended scalars

---
+++ src/main/java/com/playground/config/GraphQLConfig.java
@@ -0,0 +1,24 @@
+package com.playground.config;
+
+import graphql.scalars.ExtendedScalars;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+@Configuration
+public class GraphQLConfig {
+    @Bean
+    public RuntimeWiringConfigurer DateTimeConfigurer() {
+        return wiringBuilder -> wiringBuilder.scalar(ExtendedScalars.DateTime);
+    }
+
+    @Bean
+    public RuntimeWiringConfigurer PositiveFloatConfigurer() {
+        return wiringBuilder -> wiringBuilder.scalar(ExtendedScalars.PositiveFloat);
+    }
+
+    @Bean
+    public RuntimeWiringConfigurer CurrencyConfigurer() {
+        return wiringBuilder -> wiringBuilder.scalar(ExtendedScalars.Currency);
+    }
+}
