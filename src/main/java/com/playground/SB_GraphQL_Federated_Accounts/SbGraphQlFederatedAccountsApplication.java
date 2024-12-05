adebb8a8538e19ab0d73acc79ce3a6b20b5423f3
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-05 12:45:19 -0500

    Initial commit

---
+++ src/main/java/com/playground/SB_GraphQL_Federated_Accounts/SbGraphQlFederatedAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.playground.SB_GraphQL_Federated_Accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbGraphQlFederatedAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbGraphQlFederatedAccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playground/SB_GraphQL_Federated_Accounts/SbGraphQlFederatedAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.SB_GraphQL_Federated_Accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbGraphQlFederatedAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
